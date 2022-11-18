fd9064a49b5a227d52dd43a902ec5553bd630d7c
vpagador <80417833+vpagador@users.noreply.github.com>
2022-11-18 15:16:46 +0000

    Update TPW_scraper.py
    scrapes flavours and sizes in a listed format and a new parameter for generate_product_dictionaries

--- TPW_scraper.py
+++ TPW_scraper.py
@@ -1,5 +1,4 @@
 from bs4 import BeautifulSoup
-from lxml import etree
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
@@ -20,16 +19,17 @@ class TPW:
     '''
     A webscraper class that has methods to interact with, scrape data from and locally store 
     data from The Protein Works website
-    
-    Parameters:
+
+    Attributes:
     ----------
     driver: selenium webdriver object
         A webdriver (Chrome) that drives the website(s) listed in the page_urls_list
 
-    Attributes:
-    ----------
     page_urls_list: list
         A list of available page urls obtained from the main website
+
+    product_urls_list: list
+            A list of obtained urls for each product
     
     product_list: list
         A list of product dictionaries
@@ -57,11 +57,11 @@ class TPW:
     get_pages()
         Grabs all the available page urls
 
-    generate_product_dict()
-        Creates the product dictionary that contains data about a product and metadata. 
+    generate_product_dictionaries()
+        Creates a dictionary for each product that contains data about the product and metadata. 
         Also initiates requests and BeautifulSoup objects for locating elements
 
-    scrape_contents(contents, soup, dom)
+    scrape_contents(contents, soup)
         Locates and grabs elements where text data is found
 
     populate_contents_dict(contents,product_name, price, 
@@ -73,20 +73,22 @@ class TPW:
     retrieve_image_link(soup,contents)
         Retrieves all src links available on the page
 
-    append_products(contents,product,id, product_list, id_list)
+    append_products(contents,product,id)
         Appends contents dictionary to the product dictionary
         Adds the product dictionary and id to the product list and id list
 
-    create_json(product_list, id_list)
+    create_json()
         Creates a json file from each product dictionary and saves it locally 
 
     download_save_images(product, item_path)
         Downloads src links from each product dictionary and saves images locally
     '''
-    def __init__(self, driver):
+    def __init__(self):
         self.driver = driver
-        self.page_urls_list = ["https://www.theproteinworks.com/vegan-wondershake",
-                                "https://www.theproteinworks.com/whey-protein-360-extreme"]
+        # empty self.product_urls_list when scraping whole website
+        self.product_urls_list = ['https://www.theproteinworks.com/whey-protein-360-extreme',
+        'https://www.theproteinworks.com/upgrade']
+        self.page_urls_list = []
         self.product_list = []
         self.id_list = []
 
@@ -122,6 +124,10 @@ class TPW:
         Scrolls to the end of page
         '''
         sleep(1)
+        self.driver.execute_script("window.scrollBy(0,1000)")
+        sleep(1)
+        self.driver.execute_script("window.scrollBy(0,1000)")
+        sleep(1)
         self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
     def close_driver(self):
@@ -137,33 +143,35 @@ class TPW:
         '''
         sleep(1)
         next_page = self.driver.find_element(by=By.XPATH, 
-                                value='//*[@id="__next"]/div/div[3]/div/div[2]/div[2]')
-        next_page = next_page.find_element(by=By.XPATH, 
-                                value='div/section[2]/div/div[2]/div[3]/div/ul/li[10]')
+                                value='//*[@id="__next"]/div/div[3]/div/div[2]/div[2]/div/'
+                                'section[2]/div/div[2]/div[3]/div/ul/li[10]')
+        next_page.click()
+        
     
     def get_pages(self):
         '''
         Grabs all the available page urls
         '''
         sleep(1)
-        for i in range(0,7):
+        for i in range(1,8):
             page_url = url + f'/page/{i}' 
             self.page_urls_list.append(page_url)
         print(self.page_urls_list)
 
-    def generate_product_dict(self):
+    def generate_product_dictionaries(self):
         '''
-        1. Creates the product dictionary that contains data about a product and metadata. 
+        1. Creates a product dictionary for each listed product. 
         2. Initiates requests and BeautifulSoup objects for locating elements
+        3. Calls on other methods to fill in dictionaries with metadata, text and image data.
 
         Parameters:
         ----------
+        product_urls_list: list
+            A list of obtained urls for each product
         response: requests object
             Parses an url to get the HTML of the website
         soup: BeautifulSoup object
             Parses request object to get BeautifulSoup HTML document
-        dom: etree object
-            Parses BeautifulSoup object to get XML tree of the HTML
         id: str
             Unique id to identify a product
         contents: dict
@@ -176,11 +184,10 @@ class TPW:
         self.create_json(self.product_list, self.id_list): method
             calls the method to create a json file from the product dictionary
         '''
-        for page_url in self.page_urls_list:
-            response = requests.get(page_url)
+        for product_url in self.product_urls_list:
+            response = requests.get(product_url)
             sleep(1)
             soup = BeautifulSoup(response.content, "html.parser")
-            dom = etree.HTML(str(soup))
             id = uuid.uuid4().hex
             # Create dictionary for each product to contain id, time scraped, text data and image data
             product = {'unique id ':id, 'time' : str(datetime.datetime.now().strftime('%c')), 
@@ -189,14 +196,12 @@ class TPW:
                                 "Sizes":[],"Flavours":[],"Description Long" :[], 
                                 "Rating Percentage" : [],"Number of Reviews":[], "Images":[]}
 
-            self.scrape_contents(contents, soup, dom)
+            self.scrape_contents(contents, soup)
             self.retrieve_image_link(soup, contents)
             self.append_products(contents,product,id)
-        
-        # call the create_json method
-        return self.create_json()
+            
 
-    def scrape_contents(self,contents, soup, dom):
+    def scrape_contents(self,contents, soup):
         '''
         Locates and grabs elements where text data is found
 
@@ -206,23 +211,20 @@ class TPW:
             Dictionary containing scraped data about the product
         soup: BeautifulSoup Object
             Parses request object to get BeautifulSoup HTML document
-        dom: etree Object
-            Parses BeautifulSoup object to get XML tree of the HTML
         '''
         sleep(1)
         # Scrape each category of text data and add to dictionary
-        product_name = soup.find('h1').text    
-        price = dom.xpath('//*[@id="__next"]/div/div[3]/div/section[1]/div/div/div[2]/section/div/div[3]/'
-                                'form/div[3]/div[1]/div/div[2]/div/span/span')[0].text
+        product_name = soup.find('h1').text  
+        price = soup.select('span.ProductItem_price_normal__wYpSr.price_normal.price_special')[0].text
         description_short = soup.find(id='product-description-short').text
-        sizes = soup.find(class_='ProductItem_size__3Ux92 size btn-group').text
-        flavours = soup.find(class_='form-select').text
+        sizes = soup.find(class_='ProductItem_size__3Ux92 size btn-group')
+        flavours = soup.find(class_='form-select')
         description_long = soup.find(class_='RRT__panel panel').text
         rating_percentage = soup.find(class_='ProductItem_rating__1WQSr').text
         number_of_reviews = soup.find(class_='ProductItem_reviews__2xbGO').text
         number_of_reviews = re.sub(r'\W+','',number_of_reviews)
         
-        self.populate_contents_dict(contents, product_name, price, description_short, sizes, 
+        return self.populate_contents_dict(contents, product_name, price, description_short, sizes, 
                         flavours,description_long, rating_percentage, number_of_reviews)
 
 
@@ -255,8 +257,8 @@ class TPW:
         contents["Product Name"].append(product_name)
         contents["Price"].append(price)
         contents["Description Short"].append(description_short)
-        contents["Sizes"].append(sizes)
-        contents["Flavours"].append(flavours)
+        [contents["Sizes"].append(options.text) for options in sizes]
+        [contents["Flavours"].append(options.text) for options in flavours]
         contents["Description Long"].append(description_long)
         contents["Rating Percentage"].append(rating_percentage)
         contents["Number of Reviews"].append(number_of_reviews)
@@ -370,18 +372,19 @@ def run_scraper():
     '''
     Creates an instance of the scraper class TPW and calls its methods in sequence
     '''
-    scraper = TPW(driver)
-    '''scraper.load_and_accept_cookies()
+    scraper = TPW()
+    scraper.load_and_accept_cookies()
     scraper.pop_up()
     scraper.scroll()
     scraper.click_next_page()
-    scraper.close_driver()
-    scraper.get_pages()'''
-    scraper.generate_product_dict()
+    scraper.get_pages()
+    scraper.generate_product_dictionaries()
+    scraper.create_json()
     scraper.close_driver()
 
 
 if __name__ == '__main__':
     print('this is running directly')
     run_scraper()
+
 
